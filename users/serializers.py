80187d7dc2ffa1af8714420efc7a2eebee32623a
алексей федулов <fab-78@mail.ru>
2025-12-26 00:47:35 +0200

    corrected user/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -9,31 +9,33 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     Сериализатор для регистрации нового пользователя.
     Обеспечивает валидацию паролей и сбор необходимых данных.
     """
+
     password = serializers.CharField(
-        write_only=True,
-        required=True,
-        validators=[validate_password]
+        write_only=True, required=True, validators=[validate_password]
     )
-    password2 = serializers.CharField(
-        write_only=True,
-        required=True
-    )
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
         fields = (
-            'email', 'password', 'password2', 'first_name', 'last_name', 'phone', 'city'
+            "email",
+            "password",
+            "password2",
+            "first_name",
+            "last_name",
+            "phone",
+            "city",
         )
         extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True},
+            "first_name": {"required": True},
+            "last_name": {"required": True},
         }
 
     def validate(self, attrs):
         """
         Проверяет совпадение паролей.
         """
-        if attrs['password'] != attrs['password2']:
+        if attrs["password"] != attrs["password2"]:
             raise serializers.ValidationError({"password": "Пароли не совпадают"})
         return attrs
 
@@ -41,7 +43,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         """
         Создает нового пользователя после успешной валидации.
         """
-        validated_data.pop('password2')
+        validated_data.pop("password2")
         user = User.objects.create_user(**validated_data)
         return user
 
@@ -51,7 +53,8 @@ class UserSerializer(serializers.ModelSerializer):
     Сериализатор для отображения информации о пользователе.
     Используется при получении данных о существующем пользователе.
     """
+
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar')
-        read_only_fields = ('id', 'email')
+        fields = ("id", "email", "first_name", "last_name", "phone", "city", "avatar")
+        read_only_fields = ("id", "email")
