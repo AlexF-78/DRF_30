82fe4c9bd90bfce4d4d9ff39ca3d2e763756a564
алексей федулов <fab-78@mail.ru>
2026-01-09 19:26:59 +0200

    modified:   lms/admin.py

--- lms/admin.py
+++ lms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscription
+from .tasks import send_course_update_notification
 
 
 # Register your models here.
@@ -9,9 +10,32 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ["name"]
     search_fields = ["name", "description"]
 
+    def save_model(self, request, obj, form, change):
+        """
+        Сохраняет курс в админке и отправляет уведомления при обновлении.
+
+        Args:
+            request: HttpRequest объект
+            obj: Сохраняемый объект Course
+            form: Форма админки
+            change: True если объект изменяется, False если создаётся новый
+        """
+        super().save_model(request, obj, form, change)
+
+        # change=True означает обновление существующего объекта
+        if change:
+            send_course_update_notification.delay(obj.id)
+
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     list_display = ["name", "course", "video_link"]
     list_filter = ["course"]
     search_fields = ["name", "description", "video_link"]
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "course", "created_at")
+    list_filter = ("course", "created_at")
+    search_fields = ("user__email", "course__name")
